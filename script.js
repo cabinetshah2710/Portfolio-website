cda95d4563599371a92364b54bb68aa86223af0d
cabinet shah <cabinetshah.11812420@gmail.com>
2022-01-21 14:08:55 +0545

    Updated modules

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Python Developer", "Blogger", "Coding Guide", "Freelancer"],
+        strings: ["Data Analyst", "Python Developer", "Blogger", "Coding Guide"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Python Developer", "Blogger", "Coding Guide", "Freelancer"],
+        strings: ["Data Analyst", "Python Developer", "Blogger", "Coding Guide"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
